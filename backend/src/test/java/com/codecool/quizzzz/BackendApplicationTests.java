cb342bfb2e2bec0307bc96d0a77489aca7f2fc2f
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-10-16 15:07:28 +0200

    generate spring boot project

---
+++ backend/src/test/java/com/codecool/quizzzz/BackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.codecool.quizzzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+
+}
