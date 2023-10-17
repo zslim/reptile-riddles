63e469f1038a47f1daaf70fa3b5545a92322fd87
Kozakadam <kar.adam.robert@gmail.com>
2023-10-17 15:27:00 +0200

    add react-router as dependency

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import styles from "./index.css";
+import "./index.css";
 
 import { Layout } from './pages/Layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      // {
+      //   path: 'quiz',
+      //   element: </>,
+      // },
+    ]
+  }
+]);
+
 root.render(
   <React.StrictMode>
-    <Layout />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
