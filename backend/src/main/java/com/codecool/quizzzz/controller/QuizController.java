0abf3aea4755c4b4c3fcbc811ab9ad70c3479672
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-10-17 15:12:07 +0200

    implement QuizController with endpoints

--- backend/src/main/java/com/codecool/quizzzz/controller/QuizController.java
+++ backend/src/main/java/com/codecool/quizzzz/controller/QuizController.java
@@ -1,9 +1,12 @@
 package com.codecool.quizzzz.controller;
 
+import com.codecool.quizzzz.controller.dto.quiz.NewQuizDTO;
+import com.codecool.quizzzz.controller.dto.quiz.QuizDTO;
 import com.codecool.quizzzz.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -13,5 +16,25 @@ public class QuizController {
   @Autowired
   public QuizController(QuizService quizService) {
     this.quizService = quizService;
+  }
+  
+  @GetMapping("/all")
+  List<QuizDTO> getAllQuiz(){
+    return quizService.getAllQuizzes();
+  }
+
+  @GetMapping("/{quizId}")
+  QuizDTO getQuizById(@PathVariable int quizId){
+    return quizService.getQuizById(quizId);
+  }
+
+  @PostMapping("/{title}")
+  int createQuiz(@PathVariable String title){
+    return quizService.createQuiz(new NewQuizDTO(title));
+  }
+
+  @DeleteMapping("/{quizId}")
+  int deleteQuiz(@PathVariable int quizId){
+    return quizService.deleteQuiz(quizId);
   }
 }
