f4bb19c78975ff54f3e96f501e9c17e0650052ea
Kozakadam <kar.adam.robert@gmail.com>
2023-10-17 21:22:16 +0200

    add main selection buttons for task/quiz

--- frontend/src/pages/TaskPage.jsx
+++ frontend/src/pages/TaskPage.jsx
@@ -1,6 +1,21 @@
 const TaskPage = () => {
     return (
-        <div className="bg-kahoot-lightpurple h-screen text-white font-bold p-60">Task</div>
+        <div className="bg-grey-100 h-screen text-white font-bold">
+            <div className="absolute bottom-14 grid gap-1 p-1 grid-cols-2 w-screen object-center">
+                <div className="h-20 w-full bg-kahoot-red place-self-center">
+                    <div className="m-5 text-white">Answer 1</div>
+                </div>
+                <div className="h-20 w-full bg-kahoot-blue place-self-center">
+                    <div className="m-5 text-white">Answer 2</div>
+                </div>
+                <div className="h-20 w-full bg-kahoot-yellow place-self-center">
+                    <div className="m-5 text-white">Answer 3</div>
+                </div>
+                <div className="h-20 w-full bg-kahoot-green place-self-center">
+                    <div className="m-5 text-white">Answer 4</div>
+                </div>
+            </div>
+        </div>
     )
 };
 
