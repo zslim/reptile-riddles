6eb1e4049ea7db6e0f480888f21820955aa2006e
Kozakadam <kar.adam.robert@gmail.com>
2023-10-19 00:13:48 +0200

    add safelist to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     extend: {
       colors: {
         'kahoot': {
-          red : '#E41C3D',
+          red: '#E41C3D',
           blue: '#1369D0',
           green: '#288A0E',
           yellow: '#D89D02',
@@ -16,14 +16,26 @@ module.exports = {
           darkblue: '#2C0F70'
         },
         'hover': {
-          red : '#DA1233',
+          red: '#DA1233',
           blue: '#095FC6',
           green: '#1E8004',
           yellow: '#CE9300',
-        } 
+        },        
+        'icon': {
+          green: '#1BA35C',
+          red: '#F44336'
+        },
       },
     },
   },
    plugins: [],
+   safelist: [{
+    pattern: /(bg|text|border|hover:bg)-kahoot-(red|blue|green|yellow|)/
+  },
+  'hover:bg-hover-red',
+  'hover:bg-hover-blue',
+  'hover:bg-hover-green',
+  'hover:bg-hover-yellow',
+  ]
 }
 
