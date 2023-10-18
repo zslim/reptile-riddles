3524fc9e3ce9ac3b769d8ddb56ac90a229fcc8a1
Kozakadam <kar.adam.robert@gmail.com>
2023-10-18 22:23:25 +0200

    add task provider

---
+++ frontend/src/controllers/taskProvider.js
@@ -0,0 +1,22 @@
+async function fetchTask(quizId, taskIndex){
+    try {
+        const httpRawRes = await fetch(`/task/quiz/${quizId}/${taskIndex}`);
+        const res = await httpRawRes.json();
+        return res;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+async function validateAnswer(answerId){
+    try {
+        const httpRawRes = await fetch(`/task/${answerId}`);
+        const res = await httpRawRes.json();
+        return res;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export default fetchTask;
