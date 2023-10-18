8c0c675ad85a6af837e459ceb868366fcf628c40
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-10-18 12:30:41 +0200

    rework Quiz model's placement in the structure

--- backend/src/main/java/com/codecool/quizzzz/service/QuizService.java
+++ backend/src/main/java/com/codecool/quizzzz/service/QuizService.java
@@ -2,10 +2,10 @@ package com.codecool.quizzzz.service;
 
 import com.codecool.quizzzz.dto.quiz.NewQuizDTO;
 import com.codecool.quizzzz.dto.quiz.QuizDTO;
-import com.codecool.quizzzz.model.Task;
 import com.codecool.quizzzz.exception.NotFoundException;
+import com.codecool.quizzzz.model.Task;
 import com.codecool.quizzzz.service.dao.quiz.QuizDAO;
-import com.codecool.quizzzz.service.dao.quiz.QuizModel;
+import com.codecool.quizzzz.model.Quiz;
 import com.codecool.quizzzz.service.dao.task.TaskDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,7 +29,7 @@ public class QuizService {
   }
 
   public QuizDTO getById(int quizId) {
-    Optional<QuizModel> result = quizDAO.getById(quizId);
+    Optional<Quiz> result = quizDAO.getById(quizId);
     if (result.isEmpty()) {
       throw new RuntimeException(String.format("The quiz with id %d doesn't exist!", quizId));
     }
@@ -40,14 +40,16 @@ public class QuizService {
     return quizDAO.create(newQuizDTO);
   }
 
-  public int deleteById(int quizId){
+  public int deleteById(int quizId) {
     Optional<Integer> optionalId = quizDAO.deleteById(quizId);
-    if (optionalId.isPresent()) return optionalId.get();
+    if (optionalId.isPresent()) {
+      return optionalId.get();
+    }
     throw new NotFoundException(String.format("The quiz with id %d doesn't exist!", quizId));
   }
 
-  private QuizDTO convertQuizModelToDTO(QuizModel quizModel) {
-    List<Integer> taskIdList = taskDAO.getAllTasksByQuiz(quizModel.id()).stream().map(Task::taskId).toList();
-    return new QuizDTO(quizModel.id(), quizModel.title(), taskIdList);
+  private QuizDTO convertQuizModelToDTO(Quiz quiz) {
+    List<Integer> taskIdList = taskDAO.getAllTasksByQuiz(quiz.id()).stream().map(Task::taskId).toList();
+    return new QuizDTO(quiz.id(), quiz.title(), taskIdList);
   }
 }
