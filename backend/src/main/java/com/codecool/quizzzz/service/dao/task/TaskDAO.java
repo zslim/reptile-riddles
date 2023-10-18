fcc08aaa81a27cf3a8e6663f50ade289e4b957bb
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-10-18 12:13:28 +0200

    make getTask methods to return Optional Task in TaskDAO interface

--- backend/src/main/java/com/codecool/quizzzz/service/dao/task/TaskDAO.java
+++ backend/src/main/java/com/codecool/quizzzz/service/dao/task/TaskDAO.java
@@ -4,12 +4,13 @@ import com.codecool.quizzzz.dto.task.NewTaskDTO;
 import com.codecool.quizzzz.model.Task;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskDAO {
   List<Task> getAllTasksByQuiz(int quizId);
   int createNewTask(int quizId, NewTaskDTO newTaskDTO);
-  Task getTask(int quizId, int taskIndex);
-  Task getTask(int taskId);
+  Optional<Task> getTask(int quizId, int taskIndex);
+  Optional<Task> getTask(int taskId);
   boolean deleteTask(int quizId, int taskIndex);
   boolean deleteTask(int taskId);
 }
