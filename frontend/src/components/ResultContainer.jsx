319693d82b0c0816229163e009925d8ffd238bbd
Kozakadam <kar.adam.robert@gmail.com>
2023-10-18 12:52:53 +0200

    change next button position

--- frontend/src/components/ResultContainer.jsx
+++ frontend/src/components/ResultContainer.jsx
@@ -12,7 +12,7 @@ const ResultContainer = ({isCorrect, setTask, setIsAnswered, style}) => {
             <div className={style + " mx-auto m-6 w-6/12 pointer-events-none border-4 " + (isCorrect ? "border-green-400" : "border-red-400")}>
                 <div className="p-6 text-white">Answer 1</div>
             </div>
-            <button onClick={() => handleTaskChange()} className="p-4 bg-zinc-400 hover:bg-zinc-500 hover:cursor-pointer" >Next</button>
+            <button onClick={() => handleTaskChange()} className="absolute right-20 p-4 bg-zinc-400 hover:bg-zinc-500 hover:cursor-pointer" >Next</button>
         </div>
     )
 }
