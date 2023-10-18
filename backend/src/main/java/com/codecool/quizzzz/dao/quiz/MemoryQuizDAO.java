566012ba179be4aab3f9f461025ba0b39fcf3471
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-10-18 08:55:48 +0200

    refactor method names, class name, and create a helper method

--- backend/src/main/java/com/codecool/quizzzz/dao/quiz/MemoryQuizDAO.java
+++ backend/src/main/java/com/codecool/quizzzz/dao/quiz/MemoryQuizDAO.java
@@ -6,34 +6,38 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class QuizDAOJdbc implements QuizDAO{
+public class MemoryQuizDAO implements QuizDAO{
   private final List<QuizModel> quizzes = new ArrayList<>();
   private static int currentId = 0;
   @Override
-  public List<QuizModel> getAllQuizzes() {
+  public List<QuizModel> getAll() {
     return new ArrayList<>(quizzes);
   }
   
   @Override
-  public Optional<QuizModel> getQuizById(int quizId) {
-    return quizzes.stream().filter(quiz -> quiz.id() == quizId).findFirst();
+  public Optional<QuizModel> getById(int quizId) {
+    return findById(quizId, quizzes);
   }
   
   @Override
-  public int createQuiz(NewQuizDTO newQuizDTO) {
+  public int create(NewQuizDTO newQuizDTO) {
     quizzes.add(new QuizModel(++currentId, newQuizDTO.title()));
     return currentId;
   }
   
   @Override
-  public QuizModel editQuiz(int quizId, String newName) {
+  public QuizModel edit(int quizId, String newName) {
     return null;
   }
   
   @Override
-  public int deleteQuiz(int quizId) {
-    Optional<QuizModel> toDelete = quizzes.stream().filter(quizModel -> quizModel.id() == quizId).findFirst();
+  public int deleteById(int quizId) {
+    Optional<QuizModel> toDelete = findById(quizId, quizzes);
     toDelete.ifPresent(quizzes::remove);
     return quizId;
+  }
+
+  private Optional<QuizModel> findById(int quizId, List<QuizModel> source){
+    return source.stream().filter(quizModel -> quizModel.id() == quizId).findFirst();
   }
 }
