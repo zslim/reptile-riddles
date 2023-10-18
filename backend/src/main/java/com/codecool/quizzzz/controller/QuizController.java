332ea70717d5af95896f2a8b76b6587eeccd9876
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-10-18 09:14:48 +0200

    we get the data from the request body instead of path variable

--- backend/src/main/java/com/codecool/quizzzz/controller/QuizController.java
+++ backend/src/main/java/com/codecool/quizzzz/controller/QuizController.java
@@ -28,9 +28,9 @@ public class QuizController {
     return quizService.getById(quizId);
   }
 
-  @PostMapping("/{title}")
-  int createQuiz(@PathVariable String title){
-    return quizService.create(new NewQuizDTO(title));
+  @PostMapping("/create")
+  int createQuiz(@RequestBody NewQuizDTO newQuizDTO){
+    return quizService.create(newQuizDTO);
   }
 
   @DeleteMapping("/{quizId}")
