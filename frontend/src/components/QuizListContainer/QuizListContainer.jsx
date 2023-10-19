5df9eb71a99a1bf51c07cd18913799feb1650d4c
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-10-19 15:56:24 +0200

    add loading animation to load state

--- frontend/src/components/QuizListContainer/QuizListContainer.jsx
+++ frontend/src/components/QuizListContainer/QuizListContainer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import QuizListElement from "../QuizListElement";
+import Loading from "../Loading";
 
 function QuizListContainer({quizList, loading}) {
   return <div className="grow">
-    {loading ? <></> : (quizList.length === 0 ? <span>No quizzes found.</span> : quizList.map(quiz => <QuizListElement
-      key={quiz.id} quiz={quiz}/>))}
+    {loading ? <Loading/> : (quizList.length === 0 ? <span>No quizzes found.</span> : quizList.map(quiz =>
+      <QuizListElement key={quiz.id} quiz={quiz}/>))}
   </div>;
 }
 
