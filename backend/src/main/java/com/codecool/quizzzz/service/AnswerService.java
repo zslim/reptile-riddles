d24c162d3101ee9bc8d6e63f80ced9646a912d9f
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-10-20 01:05:54 +0200

    implement create empty answer option in Backend

--- backend/src/main/java/com/codecool/quizzzz/service/AnswerService.java
+++ backend/src/main/java/com/codecool/quizzzz/service/AnswerService.java
@@ -1,6 +1,7 @@
-package com.codecool.quizzzz.service.dao;
+package com.codecool.quizzzz.service;
 
 import com.codecool.quizzzz.dto.answer.DetailedAnswerDTO;
+import com.codecool.quizzzz.dto.answer.NewAnswerDTO;
 import com.codecool.quizzzz.exception.NotFoundException;
 import com.codecool.quizzzz.service.dao.task.AnswerDAO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,15 @@ public class AnswerService {
     this.answerDAO = answerDAO;
   }
 
-  public int updateAnswer(DetailedAnswerDTO detailedAnswerDTO) {
+  public int create(int taskId, NewAnswerDTO newAnswerDTO) {
+    return answerDAO.addAnswerToTask(taskId, newAnswerDTO);
+  }
+
+  public int create(int taskId) {
+    return answerDAO.addAnswerToTask(taskId, new NewAnswerDTO("", false));
+  }
+
+  public int update(DetailedAnswerDTO detailedAnswerDTO) {
     Optional<Integer> id = answerDAO.updateAnswer(detailedAnswerDTO);
     if (id.isPresent()) {
       return id.get();
@@ -25,7 +34,7 @@ public class AnswerService {
     throw new NotFoundException(String.format("There is no answer with answerId: %d", detailedAnswerDTO.answerId()));
   }
 
-  public boolean checkIfAnswerIsCorrect(int answerId) {
+  public boolean checkIfCorrect(int answerId) {
     return answerDAO.checkIfAnswerIsCorrect(answerId);
   }
 }
