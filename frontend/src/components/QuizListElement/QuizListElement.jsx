77df7372d88ca72517a61db69b89a5b8f46d3d8f
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-10-19 14:50:39 +0200

    change list element text color

--- frontend/src/components/QuizListElement/QuizListElement.jsx
+++ frontend/src/components/QuizListElement/QuizListElement.jsx
@@ -9,7 +9,7 @@ function QuizListElement({quiz}) {
   };
 
   return (<div className="flex flex-row border-2 m-2 p-1 rounded-md">
-    <span className="grow flex align-middle text-lg pl-2 items-center">{quiz.title}</span>
+    <span className="grow flex align-middle text-lg pl-2 items-center text-white">{quiz.title}</span>
     <button className="bg-red-400 p-1 m-1 w-20 rounded-full">Delete</button>
     <button className="bg-yellow-400 p-1 m-1 w-20 rounded-full">Edit</button>
     <button className="bg-green-400 p-1 m-1 w-20 rounded-full" onClick={() => handlePlayClick(quiz.id)}>Play</button>
