69fa420379bedda9de8074ac3c7808c58857c0d5
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-10-19 13:04:34 +0200

    add onClick to Play button

--- frontend/src/components/QuizListElement/QuizListElement.jsx
+++ frontend/src/components/QuizListElement/QuizListElement.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 function QuizListElement({quiz}) {
-  return (
-    <div key={quiz.id} className="flex flex-row border-2 m-2 p-1 rounded-md">
-      <span className="grow flex align-middle text-lg pl-2 items-center">{quiz.title}</span>
-      <button className="bg-red-400 p-1 m-1 w-20 rounded-full">Delete</button>
-      <button className="bg-yellow-400 p-1 m-1 w-20 rounded-full">Edit</button>
-      <button className="bg-green-400 p-1 m-1 w-20 rounded-full">Play</button>
-    </div>
-  );
+  const navigate = useNavigate();
+
+  const handlePlayClick = (id) => {
+    navigate(`/game/quiz/${id}`);
+  };
+
+  return (<div key={quiz.id} className="flex flex-row border-2 m-2 p-1 rounded-md">
+    <span className="grow flex align-middle text-lg pl-2 items-center">{quiz.title}</span>
+    <button className="bg-red-400 p-1 m-1 w-20 rounded-full">Delete</button>
+    <button className="bg-yellow-400 p-1 m-1 w-20 rounded-full">Edit</button>
+    <button className="bg-green-400 p-1 m-1 w-20 rounded-full" onClick={() => handlePlayClick(quiz.id)}>Play</button>
+  </div>);
 }
 
 export default QuizListElement;
