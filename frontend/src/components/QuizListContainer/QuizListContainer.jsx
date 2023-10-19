1cb5943bdfe12c3a2632b55415f92dad99336043
Kozakadam <kar.adam.robert@gmail.com>
2023-10-20 01:28:40 +0200

    add add quiz option

--- frontend/src/components/QuizListContainer/QuizListContainer.jsx
+++ frontend/src/components/QuizListContainer/QuizListContainer.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import QuizListElement from "../QuizListElement";
 import Loader from "../Loading/Loader";
+import { useNavigate } from "react-router-dom";
 
 function QuizListContainer({quizList, loading, setQuizList}) {
-
+ const navigate = useNavigate();
   function deleteQuiz(quizId){
     const newQuizList = quizList.filter((q) => q.id !== quizId);
     setQuizList(newQuizList);
+  }
+  async function createQuiz(){{
+      const httpRes = await fetch(`/quiz/create`, {
+        method: "POST",
+        body: JSON.stringify({
+          "title": ""
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      const newQuizId = await httpRes.json();
+      console.log(newQuizId);
+      navigate(`/quizform/${newQuizId}`);
+    }
   }
 
   return <div className="grow pt-16">
@@ -14,7 +30,7 @@ function QuizListContainer({quizList, loading, setQuizList}) {
       : (quizList.length === 0 ? <span>No quizzes found.</span> : quizList.map(quiz => <QuizListElement
       key={quiz.id} quiz={quiz} deleteQuiz={deleteQuiz}/>))
     }
-    <button className="bg-green-400 hover:bg-green-500 p-1 m-1 w-32 rounded-full text-black">Add Quiz</button>
+    <button className="bg-green-400 hover:bg-green-500 p-1 m-1 w-32 rounded-full text-black" onClick={() => createQuiz()}>Add Quiz</button>
   </div>;
 }
 
