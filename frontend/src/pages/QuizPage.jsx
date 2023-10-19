50129729f042a001c7e9e7cc444621901aef9d74
Kozakadam <kar.adam.robert@gmail.com>
2023-10-19 13:18:26 +0200

    add quiz page

---
+++ frontend/src/pages/QuizPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from "react-router-dom";
+
+const QuizPage = () => {
+
+  const { quizId } = useParams();
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default QuizPage;
