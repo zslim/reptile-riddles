4efbe3cfebf6ee0d51877169373c0483437dc938
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-10-19 12:11:29 +0200

    style QuizListElement

--- frontend/src/components/QuizListElement/QuizListElement.jsx
+++ frontend/src/components/QuizListElement/QuizListElement.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 function QuizListElement({quiz}) {
   return (
-    <div key={quiz.id}>
-      <h1>{quiz.title}</h1>
+    <div key={quiz.id} className="flex flex-row border-2 m-2 p-1 rounded">
+      <span className="grow flex align-middle text-lg items-center">{quiz.title}</span>
+      <button className="bg-red-400 p-1 m-1 w-20 rounded-full">Delete</button>
+      <button className="bg-yellow-400 p-1 m-1 w-20 rounded-full">Edit</button>
+      <button className="bg-green-400 p-1 m-1 w-20 rounded-full">Play</button>
     </div>
   );
 }
