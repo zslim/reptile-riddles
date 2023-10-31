4455b2c848278ac89753b7ae1802e4532f5e3710
Kozakadam <kar.adam.robert@gmail.com>
2023-10-31 21:21:14 +0100

    remove excess bracketing

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import LogoMain from '../../images/logo_big.png';
 
-function Homepage() {
+const Homepage = () => {
   return (
     <>
-      <div className={"h-[calc(100%-52px)] fixed flex bg-inherit w-full"}>
-        <div className={"max-h-full max-w-full flex mx-auto"}>
-          <img src={LogoMain} alt={"main_logo"} className={"mx-auto"}/>
+      <div className="h-[calc(100%-52px)] fixed flex bg-inherit w-full">
+        <div className="max-h-full max-w-full flex mx-auto">
+          <img src={LogoMain} alt={"main_logo"} className="mx-auto"/>
         </div>
       </div>
     </>
