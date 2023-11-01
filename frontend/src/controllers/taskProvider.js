281237b56dbc63f36a2cfaa8ad94d33fc6e9524d
Kozakadam <kar.adam.robert@gmail.com>
2023-11-01 13:29:08 +0100

    add save and delete request for task provider

--- frontend/src/controllers/taskProvider.js
+++ frontend/src/controllers/taskProvider.js
@@ -27,6 +27,17 @@ async function saveEmptyTask(quizId) {
   return await res.json();
 }
 
+async function saveTask(quizId, task) {
+  const res = await fetch(`/task/quiz/${quizId}`, {
+    method: "POST",
+    body: JSON.stringify(task),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  return await res.json();
+}
+
 async function updateQuestion(question, taskId) {
   const httpRes = await fetch(`/task/${taskId}`, {
     method: "PATCH", body: JSON.stringify({
@@ -38,11 +49,22 @@ async function updateQuestion(question, taskId) {
   return await httpRes.json();
 }
 
+async function deleteTaskById(taskId) {
+  const res = await fetch(`/task/${taskId}`, {
+    method: "DELETE", headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  return await res.json();
+}
+
 module.exports = {
   fetchTask: fetchTaskByIndex,
   fetchTasksByQuizId,
   fetchDetailedTasksByQuizId,
   saveEmptyTask,
   fetchTaskById,
-  updateQuestion
+  updateQuestion,
+  saveTask,
+  deleteTaskById
 };
