a2f572d886cb425c7201f1d3be787b54cc24bfb5
Kozakadam <kar.adam.robert@gmail.com>
2023-11-01 21:38:14 +0100

    add loading back to quiz editor

--- frontend/src/pages/QuizEditor/QuizEditor.jsx
+++ frontend/src/pages/QuizEditor/QuizEditor.jsx
@@ -149,8 +149,8 @@ const QuizEditor = () => {
 
   return (
     <>
-      {/*{loading ? <Loading/>*/}
-      {/*  : */}
+      {loading ? <Loading/>
+        :
         <div className="h-[calc(100%-52px)] fixed bg-inherit w-full grid grid-cols-12">
           <div className="max-h-4/6 p-2 pl-6 mt-10 grid grid-cols-1 col-span-2 auto-rows-min">
             <div className="max-h-[65vh] overflow-auto pt-1 pb-1 grid grid-cols-1 gap-1">
@@ -198,7 +198,7 @@ const QuizEditor = () => {
             </div>
           </div>
         </div>
-      {/*}*/}
+      }
     </>
   );
 };
