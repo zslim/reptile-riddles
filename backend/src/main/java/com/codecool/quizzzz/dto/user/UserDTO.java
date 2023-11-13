69c829444cf91cea2a8fb256f40cae55e65a6347
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-11-13 13:59:47 +0100

    change name to username, and change order

--- backend/src/main/java/com/codecool/quizzzz/dto/user/UserDTO.java
+++ backend/src/main/java/com/codecool/quizzzz/dto/user/UserDTO.java
@@ -1,4 +1,4 @@
 package com.codecool.quizzzz.dto.user;
 
-public record UserDTO (long id, String name, String email, String password){
+public record UserDTO (long id, String username, String password, String email){
 }
