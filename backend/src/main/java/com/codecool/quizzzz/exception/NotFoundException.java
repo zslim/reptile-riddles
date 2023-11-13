25b373d788e109d9d4f52ca2028cfa0f2a3c8287
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-11-13 13:47:36 +0100

    add constructor to NotFoundException

--- backend/src/main/java/com/codecool/quizzzz/exception/NotFoundException.java
+++ backend/src/main/java/com/codecool/quizzzz/exception/NotFoundException.java
@@ -8,4 +8,8 @@ public class NotFoundException extends RuntimeException {
   public NotFoundException(String message, Throwable cause) {
     super(message, cause);
   }
+
+  public NotFoundException(String subject, long id) {
+    super(String.format(subject + " not found with id: " + id));
+  }
 }
