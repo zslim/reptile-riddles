1df8edc4e8c4ef1e3d81e6ab72dac0b25c3a5904
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-11-14 14:46:08 +0100

    update UserEntity properties

---
+++ backend/src/main/java/com/codecool/quizzzz/model/user/UserEntity.java
@@ -0,0 +1,31 @@
+package com.codecool.quizzzz.model.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "user_entity")
+public class UserEntity {
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String username;
+  private String email;
+  private String password;
+  @ManyToMany(cascade = CascadeType.ALL)
+  @JoinTable(
+          name = "role_user_entity",
+          joinColumns = {@JoinColumn(name = "role_id")},
+          inverseJoinColumns = {@JoinColumn(name = "user_id")}
+  )
+  private Set<Role> roles;
+}
