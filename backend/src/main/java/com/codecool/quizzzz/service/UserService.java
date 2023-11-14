8072049d4036cc4d971cc7944e09e12dc4701901
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-11-14 15:00:44 +0100

    rename

--- backend/src/main/java/com/codecool/quizzzz/service/UserService.java
+++ backend/src/main/java/com/codecool/quizzzz/service/UserService.java
@@ -3,24 +3,35 @@ package com.codecool.quizzzz.service;
 import com.codecool.quizzzz.dto.user.NewUserDTO;
 import com.codecool.quizzzz.dto.user.UserDTO;
 import com.codecool.quizzzz.exception.NotFoundException;
-import com.codecool.quizzzz.model.User;
+import com.codecool.quizzzz.model.user.RoleEnum;
+import com.codecool.quizzzz.model.user.UserEntity;
+import com.codecool.quizzzz.service.repository.RoleRepository;
 import com.codecool.quizzzz.service.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class UserService {
   private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final RoleRepository repository;
 
-  public UserService(UserRepository userRepository) {
+  public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository repository) {
     this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
+    this.repository = repository;
   }
 
   public void create(NewUserDTO newUserDTO) {
-    userRepository.save(newUserDTOtoModel(newUserDTO)); // TODO: validate user details, like @ or password length
+    UserEntity user = newUserDTOtoModel(newUserDTO);
+    user.setRoles(Set.of(repository.findByName(RoleEnum.ROLE_USER).orElseThrow(()-> new NotFoundException("user"))));
+    userRepository.save(user);
   }
 
   public UserDTO getById(long id) {
-    User user = userRepository.findById(id).orElseThrow(() -> new NotFoundException("User", id));
+    UserEntity user = userRepository.findById(id).orElseThrow(() -> new NotFoundException("User", id));
     return modelToUserDTO(user);
   }
 
@@ -28,15 +39,15 @@ public class UserService {
     userRepository.deleteById(id);
   }
 
-  private User newUserDTOtoModel(NewUserDTO newUserDTO) {
-    return User.builder()
-               .email(newUserDTO.email())
-               .username(newUserDTO.username())
-               .password(newUserDTO.password())
-               .build();
+  private UserEntity newUserDTOtoModel(NewUserDTO newUserDTO) {
+    return UserEntity.builder()
+                     .email(newUserDTO.email())
+                     .username(newUserDTO.username())
+                     .password(passwordEncoder.encode(newUserDTO.password()))
+                     .build();
   }
 
-  private UserDTO modelToUserDTO(User user) {
+  private UserDTO modelToUserDTO(UserEntity user) {
     return new UserDTO(user.getId(), user.getUsername(), user.getEmail(), user.getPassword());
   }
 }
