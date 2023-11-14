362144875f83a3b70aecf14dd53d39cd304a1302
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-11-14 14:59:29 +0100

    create AuthTokenFilter class

---
+++ backend/src/main/java/com/codecool/quizzzz/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,62 @@
+package com.codecool.quizzzz.security.jwt;
+
+import com.codecool.quizzzz.service.logger.Logger;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+  private final JwtUtils jwtUtils;
+  private final UserDetailsService userDetailsService;
+  private final Logger logger;
+
+
+  public AuthTokenFilter(JwtUtils jwtUtils, UserDetailsService userDetailsService, Logger logger) {
+    this.jwtUtils = jwtUtils;
+    this.userDetailsService = userDetailsService;
+    this.logger = logger;
+  }
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                  FilterChain filterChain) throws ServletException, IOException {
+    try {
+      String jwt = parseJwt(request);
+      if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+        String username = jwtUtils.getUserNameFromJwtToken(jwt);
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,
+                                                                                                          null,
+                                                                                                          userDetails.getAuthorities());
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+      }
+    }
+    catch (Exception e) {
+      logger.logError(e.getMessage(), "Cannot set user authentication");
+    }
+
+    filterChain.doFilter(request, response);
+  }
+
+  private String parseJwt(HttpServletRequest request){
+    String headerAuth = request.getHeader("Authorization");
+    if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")){
+      return headerAuth.substring(7);
+    }
+    return null;
+  }
+}
