939eb2fb6b0f9e05d369f7a335e0fddcb59629d1
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-11-14 14:58:02 +0100

    create role repo

---
+++ backend/src/main/java/com/codecool/quizzzz/service/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.codecool.quizzzz.service.repository;
+
+import com.codecool.quizzzz.model.user.Role;
+import com.codecool.quizzzz.model.user.RoleEnum;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(RoleEnum name);
+}
