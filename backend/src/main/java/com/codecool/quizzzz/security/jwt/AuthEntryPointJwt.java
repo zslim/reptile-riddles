94861173548a55331e2e818df441ae6b56c2dae1
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-11-15 18:46:25 +0100

    reformat

--- backend/src/main/java/com/codecool/quizzzz/security/jwt/AuthEntryPointJwt.java
+++ backend/src/main/java/com/codecool/quizzzz/security/jwt/AuthEntryPointJwt.java
@@ -4,7 +4,6 @@ import com.codecool.quizzzz.service.logger.Logger;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jdk.jfr.Category;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
