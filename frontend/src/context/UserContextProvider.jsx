a468b37ca0c528a90f1a71e968218751750d16fd
Kozakadam <kar.adam.robert@gmail.com>
2023-11-16 18:41:17 +0100

    add logout method

--- frontend/src/context/UserContextProvider.jsx
+++ frontend/src/context/UserContextProvider.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { getCredentials, userLogin } from "../controllers/userProvider";
+import { getCredentials, userLogin, userLogout } from "../controllers/userProvider";
+import authenticate from "./authenticator";
 
 const UserContext = createContext({});
 const NO_USER = {username: "", roles: []};
@@ -8,26 +9,26 @@ export const UserContextProvider = ({children}) => {
   const [user, setUser] = useState({...NO_USER});
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function checkCredentials() {
-      try {
-        setLoading(true);
-        const userCredentials = await getCredentials();
-        if (userCredentials !== null) {
-          setUser(userCredentials);
-        }
-        else {
-          setUser({...NO_USER});
-        }
+  async function checkCredentials() {
+    try {
+      setLoading(true);
+      const userCredentials = await getCredentials();
+      if (userCredentials !== null) {
+        setUser(userCredentials);
       }
-      catch (e) {
-        console.error(e);
-      }
-      finally {
-        setLoading(false);
+      else {
+        setUser({...NO_USER});
       }
     }
+    catch (e) {
+      console.error(e);
+    }
+    finally {
+      setLoading(false);
+    }
+  }
 
+  useEffect(() => {
     checkCredentials();
   }, []);
 
@@ -42,8 +43,23 @@ export const UserContextProvider = ({children}) => {
     }
   }
 
+  async function logout() {
+    try {
+      setLoading(true);
+      const status = await userLogout();
+
+      setUser({...NO_USER});
+    } catch (e){
+      console.error(e);
+    }
+    finally {
+      await checkCredentials();
+      setLoading(false);
+    }
+  }
+
   return (
-    <UserContext.Provider value={{user, login}}>
+    <UserContext.Provider value={{user, login, logout}}>
       {!loading && children}
     </UserContext.Provider>
   );
