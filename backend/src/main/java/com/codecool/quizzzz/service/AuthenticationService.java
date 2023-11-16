0cfc7cdc339ce2f74edc1a2e8815dd5aba37660f
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-11-16 10:32:35 +0100

    add getCredentials method to AuthenticationService

--- backend/src/main/java/com/codecool/quizzzz/service/AuthenticationService.java
+++ backend/src/main/java/com/codecool/quizzzz/service/AuthenticationService.java
@@ -2,6 +2,7 @@ package com.codecool.quizzzz.service;
 
 import com.codecool.quizzzz.dto.user.LoginDTO;
 import com.codecool.quizzzz.dto.user.UserInfoDTO;
+import com.codecool.quizzzz.dto.user.UserInfoJwtDTO;
 import com.codecool.quizzzz.security.jwt.JwtUtils;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class AuthenticationService {
@@ -23,7 +25,7 @@ public class AuthenticationService {
     this.jwtUtils = jwtUtils;
   }
 
-  public UserInfoDTO login(LoginDTO loginDTO) {
+  public UserInfoJwtDTO login(LoginDTO loginDTO) {
     Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDTO.username(),
                                                                                                                loginDTO.password()));
     SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -31,8 +33,15 @@ public class AuthenticationService {
     User user = (User) authentication.getPrincipal();
     String jwt = jwtUtils.generateJwtToken(authentication);
     List<String> roles = user.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
-    return new UserInfoDTO(jwt, loginDTO.username(), roles);
+    return new UserInfoJwtDTO(jwt, loginDTO.username(), roles);
   }
 
   // TODO: guest login
+
+  public UserInfoDTO getCredentials() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    String username = authentication.getPrincipal().toString();
+    List<String> roles = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+    return new UserInfoDTO(username, roles);
+  }
 }
