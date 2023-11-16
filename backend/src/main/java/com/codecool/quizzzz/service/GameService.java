7153746048fd05b27f96440b931a37f775502570
Tamas Molnar <125677315+Tomocza@users.noreply.github.com>
2023-11-16 10:58:34 +0100

    change return type of findGameById to optional

--- backend/src/main/java/com/codecool/quizzzz/service/GameService.java
+++ backend/src/main/java/com/codecool/quizzzz/service/GameService.java
@@ -5,6 +5,7 @@ import com.codecool.quizzzz.dto.quiz.GameQuizDTO;
 import com.codecool.quizzzz.dto.task.GameTaskDTO;
 import com.codecool.quizzzz.dto.user.NewPlayerDTO;
 import com.codecool.quizzzz.dto.user.PlayerDTO;
+import com.codecool.quizzzz.exception.NotFoundException;
 import com.codecool.quizzzz.model.*;
 import com.codecool.quizzzz.service.repository.GameRepository;
 import com.codecool.quizzzz.service.repository.QuizRepository;
@@ -13,7 +14,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class GameService {
@@ -27,7 +27,7 @@ public class GameService {
   }
 
   public GameQuizDTO createGame(Long quizId) {
-    Quiz quiz = quizRepository.findById(quizId).orElseThrow();
+    Quiz quiz = quizRepository.findById(quizId).orElseThrow(() -> new NotFoundException("No quiz found with this id!"));
     Game newGame = new Game(quiz);
     gameRepository.addGame(newGame);
     return new GameQuizDTO(newGame.getGameId(), quiz.getTitle(), quiz.getTaskCount());
@@ -35,20 +35,24 @@ public class GameService {
 
   // TODO: get information about how players will be stored (gen. from user? what abt guest users? spring sec tokens?)
   public Long joinToGame(Long gameId, NewPlayerDTO newPlayerDTO) {
-    Game game = gameRepository.findGameById(gameId);
-    game.addPlayer(new Player(newPlayerDTO.playerName()));
-    return 1L;
+    Game game = gameRepository.findGameById(gameId)
+                              .orElseThrow(() -> new NotFoundException("No game found with this id!"));
+    Player player = new Player(newPlayerDTO.playerName());
+    game.addPlayer(player);
+    return player.getPlayerId();
   }
 
   public GameTaskDTO getNextTaskFromGame(Long gameId) {
-    Game game = gameRepository.findGameById(gameId);
+    Game game = gameRepository.findGameById(gameId)
+                              .orElseThrow(() -> new NotFoundException("No game found with this id!"));
     Task nextTask = game.advanceToNextTask();
     game.setDeadline(LocalDateTime.now().plusSeconds((long) nextTask.getTimeLimit() + Game.DEADLINE_OFFSET));
     return taskToGameTaskDTO(nextTask, game.getDeadline(), game.getCurrentTaskIndex());
   }
 
   public Boolean handleAnswerSubmit(Long gameId, Long playerId, Long answerId) {
-    Game game = gameRepository.findGameById(gameId);
+    Game game = gameRepository.findGameById(gameId)
+                              .orElseThrow(() -> new NotFoundException("No game found with this id!"));
     Player player = game.getPlayerById(playerId);
     Answer answer = game.getCurrentTask().getAnswerById(answerId).orElseThrow();
     int scoreGain = answer.isCorrect() ? game.calculateScoreGain(LocalDateTime.now()) : 0;
@@ -58,7 +62,8 @@ public class GameService {
   }
 
   public List<PlayerDTO> getResult(Long gameId) {
-    Game game = gameRepository.findGameById(gameId);
+    Game game = gameRepository.findGameById(gameId)
+                              .orElseThrow(() -> new NotFoundException("No game found with this id!"));
     return game.getTopPlayers(5)
                .stream()
                .map(player -> new PlayerDTO(player.getPlayerId(), player.getScore(), player.getPlayerName()))
@@ -71,8 +76,6 @@ public class GameService {
   }
 
   private List<GameAnswerDTO> getGameAnswerDTOList(List<Answer> answers) {
-    return answers.stream()
-                  .map((answer -> new GameAnswerDTO(answer.getId(), answer.getText())))
-                  .toList();
+    return answers.stream().map((answer -> new GameAnswerDTO(answer.getId(), answer.getText()))).toList();
   }
 }
