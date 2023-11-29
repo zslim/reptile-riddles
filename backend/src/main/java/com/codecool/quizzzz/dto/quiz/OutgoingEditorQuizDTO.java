276fac47e0d8d2949ee036ac95bc5449beb58a04
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-11-29 10:32:20 +0100

    extend quiz editor DTOs with categories

--- backend/src/main/java/com/codecool/quizzzz/dto/quiz/OutgoingEditorQuizDTO.java
+++ backend/src/main/java/com/codecool/quizzzz/dto/quiz/OutgoingEditorQuizDTO.java
@@ -4,7 +4,8 @@ import com.codecool.quizzzz.dto.task.BriefTaskDTO;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 public record OutgoingEditorQuizDTO(Long id, String title, List<BriefTaskDTO> taskList, LocalDateTime createdAt,
-                                    LocalDateTime modifiedAt) {
+                                    LocalDateTime modifiedAt, Set<String> categories) {
 }
