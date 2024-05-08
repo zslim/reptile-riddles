55fd8837f73e74427f87e62b74bf1c43d8b35348
Zsófia Limbek <zsofia.limbek@codecool.com>
2024-05-08 16:04:02 +0200

    fixed proxy to work in docker composed environment

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(createProxyMiddleware("/api/**", { target: "http://localhost:8080" }));
+  app.use(createProxyMiddleware("/api/**", { target: "http://backend:8080" }));
   // app.use(createProxyMiddleware("/socket", { target: "ws://10.44.7.1:9090", ws: true, changeOrigin: true }));
 };
 
