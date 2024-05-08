2faf8d8fe489f862979a0d4a6ae1032e684ccd6c
Zsófia Limbek <zsofia.limbek@codecool.com>
2024-05-08 17:25:57 +0200

    created render blueprint, adjusted env var handling

--- backend/src/main/java/com/codecool/quizzzz/service/repository/GameRepository.java
+++ backend/src/main/java/com/codecool/quizzzz/service/repository/GameRepository.java
@@ -6,6 +6,7 @@ import com.codecool.quizzzz.model.Game;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface GameRepository {
   void addGame(Game game);
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,7 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(createProxyMiddleware("/api/**", { target: "http://backend:8080" }));
+  app.use(createProxyMiddleware("/api/**", { target: `${process.env.BE_HOST}:${process.env.BE_PORT}` }));
   // app.use(createProxyMiddleware("/socket", { target: "ws://10.44.7.1:9090", ws: true, changeOrigin: true }));
 };
-
